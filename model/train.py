e5d80e9e0d91aafc958c4f8beb4e3e6f43f4b5b2
Adamo Fapohunda <adamofapohunda@gmail.com>
2020-07-05 12:06:23 +0200

    dependecies update

--- model/train.py
+++ model/train.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 __author__ = 'Tony Beltramelli - www.tonybeltramelli.com'
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
 import sys
