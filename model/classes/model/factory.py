283e14c749a6a9fe0d4d9198daeff43b9fc46fb3
Elidor00 <filippo.bartolini9@gmail.com>
2020-08-13 17:42:21 +0200

    Added gru model

--- model/classes/model/factory.py
+++ model/classes/model/factory.py
@@ -1,15 +1,16 @@
 from model.classes.model.pix2code import pix2code
 from model.classes.model.pix2codebilstm import pix2codeBiLSTM
+from model.classes.model.pix2codegru import pix2codegru
 
 
 class ModelFactory:
-	@staticmethod
-	def create_model(model, input_shape, output_size, output_path):
-		if model == 'base':
-			return pix2code(input_shape, output_size, output_path)
+    @staticmethod
+    def create_model(model, input_shape, output_size, output_path):
+        if model == 'base':
+            return pix2code(input_shape, output_size, output_path)
 
-		elif model == 'bilstm':
-			return pix2codeBiLSTM(input_shape, output_size, output_path)
+        elif model == 'bilstm':
+            return pix2codeBiLSTM(input_shape, output_size, output_path)
 
-		# elif name == 'GRU':
-		#     return gru(input_shape, output_size, output_path)
+        elif model == 'gru':
+            return pix2codegru(input_shape, output_size, output_path)
