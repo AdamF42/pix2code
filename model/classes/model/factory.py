b61a7ee9344e8f5ea60b61c032d702a85419ebf4
Adamo Fapohunda <adamofapohunda@gmail.com>
2020-10-09 18:10:05 +0200

    need to complete the arch since the Repeat Vector do not work

--- model/classes/model/factory.py
+++ model/classes/model/factory.py
@@ -1,6 +1,7 @@
 from classes.model.pix2code import pix2code
 from classes.model.pix2codebilstm import pix2codeBiLSTM
 from classes.model.pix2codegru import pix2codegru
+from classes.model.pix2codeResNet import pix2codeResNet
 
 
 class ModelFactory:
@@ -12,5 +13,7 @@ class ModelFactory:
             return pix2codeBiLSTM(input_shape, output_size, output_path, encoding_type)
         elif model == 'gru':
             return pix2codegru(input_shape, output_size, output_path, encoding_type)
+        elif model == 'resnet':
+            return pix2codeResNet(input_shape, output_size, output_path, encoding_type)
         else:
             raise Exception("Model not found")
---
+++ model/classes/model/pix2codeResNet.py
@@ -0,0 +1,56 @@
+from __future__ import absolute_import
+
+__author__ = 'Tony Beltramelli - www.tonybeltramelli.com'
+
+from keras import *
+from keras.applications import ResNet152
+from keras.layers import Dense, RepeatVector, LSTM, concatenate
+
+from .AModel import *
+from .Config import *
+
+
+class pix2codeResNet(AModel):
+
+    def __init__(self, input_shape, output_size, output_path, encoding_type):
+        AModel.__init__(self, input_shape, output_size, output_path, encoding_type)
+        self.name = "pix2code"
+
+        image_model = Sequential()
+        image_model.add(ResNet152(include_top=False, weights=None, input_shape=input_shape))
+        image_model.add(RepeatVector(CONTEXT_LENGTH))
+
+        visual_input = Input(shape=input_shape)
+        encoded_image = image_model(visual_input)
+
+        language_model = Sequential()
+        language_model.add(LSTM(128, return_sequences=True, input_shape=(CONTEXT_LENGTH, output_size)))
+        language_model.add(LSTM(128, return_sequences=True))
+
+        textual_input = Input(shape=(CONTEXT_LENGTH, output_size))
+        encoded_text = language_model(textual_input)
+
+        # should be(None, 48, 1024) (None, 48, 128)
+        decoder = concatenate([encoded_image, encoded_text]) # (None, 8, 8, 2048) (None, 48, 128)
+
+        decoder = LSTM(512, return_sequences=True)(decoder)
+        decoder = LSTM(512, return_sequences=False)(decoder)
+        decoder = Dense(output_size, activation='softmax')(decoder)
+
+        self.model = Model(inputs=[visual_input, textual_input], outputs=decoder)
+        self.compile()
+
+    def fit(self, images, partial_captions, next_words):
+        self.model.fit([images, partial_captions], next_words, shuffle=False, epochs=EPOCHS,
+                       batch_size=BATCH_SIZE, verbose=1)
+        self.save()
+
+    def fit_generator(self, generator, steps_per_epoch, epochs=EPOCHS):
+        self.model.fit_generator(generator, steps_per_epoch=steps_per_epoch, epochs=epochs, verbose=1)
+        self.save()
+
+    def predict_batch(self, images, partial_captions):
+        return self.model.predict([images, partial_captions], verbose=1)
+
+    def predict(self, image, partial_caption):
+        return self.model.predict([image, partial_caption], verbose=0)[0]
